bba9a3c695a4f738510f65604737fbc37477f3ee
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-11-08 17:35:12 +0530

    Add files via upload

---
+++ Student.sql
@@ -0,0 +1,13 @@
+select * from product
+alter table  product alter column Copanyname varchar(40)
+
+select * from student
+create  table student(
+rollno int primary key identity(1,1),
+name varchar(20),
+sub1 varchar(20),
+sub2 varchar(20),
+sub3 varchar(20),
+percentage decimal)
+
+
